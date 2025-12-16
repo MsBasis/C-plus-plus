592d224dc5e0c42f66dd7220a55617442148e25f
MrBasis <matikgawin@gmail.com>
2025-12-16 23:48:30 +0100

    prepared map pod sprawdzanie uzytkownika

--- admin.cpp
+++ admin.cpp
@@ -1,7 +1,8 @@
 //sprawdzanie logowanie czy uzytkownik istnieje w systemie
 
 #include <iostream>
-#include <vector>
+#include <vector> //jak lista w pythonie
+#include <map> //jak slownik w pythonie
 using namespace std;
 
 
@@ -14,16 +15,24 @@ vector<string> logowanie(){ //mozna zwracac dane w rozncyh foramtach ale trzbe t
     vector<string> pasy = {login, haslo};
     return pasy;
 }
+map<string, vector<string>> database(){
+    map<string, vector<string>> users = {
+        {"MrBasis",{"MrBasis","1234","Admin"}},
+        {"Eva",{"Eva","67","user"}}
+    };
+    return users;
+}
 
 void sprawdzanie(){
+    map<string, vector<string>> baza = database();
+    vector<string> user = logowanie();
     
 }
 
 
 
 int main(){
-    vector<string> x = logowanie();
-    cout << "nazwa: "<<x[0];
+    //vector<string> x = logowanie();
     return 0;
 }
 
