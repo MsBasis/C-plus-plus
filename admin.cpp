e1dae95dd8f16c76b99dd09730671963b629e938
MrBasis <matikgawin@gmail.com>
2026-01-03 16:47:08 +0100

    working checikng admin status

--- admin.cpp
+++ admin.cpp
@@ -5,7 +5,7 @@
 #include <map> //jak slownik w pythonie
 using namespace std;
 
-
+//przyjmujr login i haslo od uzytkownika i zwraca jako vector
 vector<string> logowanie(){ //mozna zwracac dane w rozncyh foramtach ale trzbe to doprecyzowac
     string login, haslo;
     cout <<"Podaj login: "<<endl;
@@ -15,21 +15,33 @@ vector<string> logowanie(){ //mozna zwracac dane w rozncyh foramtach ale trzbe t
     vector<string> pasy = {login, haslo};
     return pasy;
 }
+//mini baza uzytkownikow
 map<string, vector<string>> database(){
     map<string, vector<string>> users = {
         {"User1",{"MrBasis","1234","Admin"}},
-        {"User2",{"Eva","67","user"}}
+        {"User2",{"Eva","67","User"}},
+        {"User3",{"Mira","21","User"}}
     };
     return users;
 }
-
+//sprawdzanie czy dany uzytkownik jest adminem czy nie
 void sprawdzanie(){
     map<string, vector<string>> baza = database();
     vector<string> user = logowanie();
     int rozmiar = baza.size();
     
-    for (int i = 0; i < rozmiar; i++) {
-        cout << "cos"<<endl;
+    for (auto &[key, vec] : baza) {
+        if (!vec.empty()) {
+            if (vec[0] == user[0] && vec[1] == user[1]) {
+                if (vec[2] == "Admin") {
+                    cout << "You're an Admin."<<endl;
+                    cout << "You've access to every file."<<endl;
+                } else {
+                    cout << "You're an User."<<endl;
+                    cout<< "You've to pay Admin 5$ to become an Admin."<<endl;
+                }
+            }
+        }
     }
 }
 
