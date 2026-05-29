b74627cdedfd8238f941ad49186beddd43bca6b6
MsBasis <matikgawin@gmail.com>
2026-05-29 23:22:45 +0200

    gpa

--- Beginner/GPA/gpa.cpp
+++ Beginner/GPA/gpa.cpp
@@ -5,23 +5,145 @@
 
 
 #include <iostream>
+#include <array>
+#include <vector>
+#include <format>
 using namespace std;
+
+struct Course {
+    string name;
+    int quant;
+    vector<float> grades;
+    float gpa;
+};
+
 
 
 // user inputs courses and grades
-void create_syllabus(){
-    string course;
-    cout << "INput course name";
-    cin >> course;
-    cout << "COurse name: " << course <<"\n";
+vector<Course> create_syllabus(int course_number){
+    vector<Course> courses(course_number); //arrays can be a made up type by me
+    for (int i = 0; i< course_number; i++){
+        string course_name;
+        int grades_number;
+        vector<float> grades;
+        float gpa = 0;
+
+        cout << "Type a course name: ";
+        cin >> course_name;
+
+        cout << "Type a number of grades in a course: ";
+        cin >> grades_number;
+        
+        
+        courses[i].name = course_name;
+        courses[i].quant = grades_number;
+
+        for (int j=0; j < grades_number; j++){
+            float grade;
+            string text = format("Input {} grade: \n", j+1);
+            cout << text;
+            cin >> grade;
+            gpa = gpa + grade;
+            grades.push_back(grade);
+        }
+        courses[i].grades = grades;
+        gpa = gpa / grades_number;
+        courses[i].gpa = gpa;
+        
+    }
+    return courses;
+}
+
+void show_gpa(vector<Course> syllabus){
+    int choose;
+    for (int i=0; i < syllabus.size();i++){
+        string text = format("[{}] {}\n",i,syllabus.at(i).name);
+        cout << text;
+    }
+    cout << "Choose number to display courses gpa: ";
+    cin >> choose;
+    string name = syllabus.at(choose).name;
+    float gpa = syllabus.at(choose).gpa;
+    string text_gpa = format("{} gpa: {}\n", name, gpa);
+    cout << text_gpa;
+}
+
+void show_cgpa(vector<Course> syllabus){
+    float cgpa = 0.0;
+    for (int i=0; i < syllabus.size(); i++){
+        cgpa = cgpa + syllabus.at(i).gpa;
+    }
+    cgpa = cgpa / syllabus.size();
+    string text = format("CGPA: {}\n", cgpa);
+    cout << text;
+}
+
+void show_grades(vector<Course> syllabus){
+    int choose;
+    for (int i=0; i < syllabus.size(); i++){
+        string text = format("[{}] {}\n",i, syllabus.at(i).name);
+        cout << text;
+    }
+    cout << "Choose a number to display courses grades: ";
+    cin >> choose;
+    for (int i=0; i < syllabus.at(choose).grades.size(); i++){
+        string text_grades = format("[{}] {}\n",i, syllabus.at(choose).grades.at(i));
+        cout << text_grades;
+    }
+
+}
+
+void loop_gpa(vector<Course> syllabus){
+    bool condition = true;
+    while (condition) {
+        int choose;
+        cout << "- - - - - - - - - - - - - - - - - - -" << endl;
+        cout << "[0] Show whole syllabus" << endl;
+        cout << "[1] Show GPA of chosen course" << endl;
+        cout << "[2] Show grades of chosen course" << endl;
+        cout << "[3] Show CGPA" << endl;
+        cout << "[4] End program" << endl;
+        cout << "Choose a number of what u wanna do: ";
+        cin >> choose;
+        cout << "- - - - - - - - - - - - - - - - - - -" << endl;
+        switch (choose) {
+            case 0:
+                for (int i=0; i < syllabus.size(); i++){
+                    string text = format("[{}] {}\n",i, syllabus.at(i).name);
+                    cout << text;
+                }
+                break;
+            case 1:
+                show_gpa(syllabus);
+                break;
+            case 2:
+                show_grades(syllabus);
+                break;
+            case 3:
+                show_cgpa(syllabus);
+                break;
+            case 4:
+                condition = false;
+                break;
+        }
+
+    }
+
 
 }
 
 
 
 
+
 int main(){
-    create_syllabus();
+    int course_number;
+    cout << "Type a number of your courses: ";
+    cin >> course_number;
+    vector<Course> syllabus = create_syllabus(course_number);
+    
+    loop_gpa(syllabus);
+
     return 0;
 }
 
